7fa0c57a0c1d3826aba3a70445c0c6585858fb61
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-03-20 23:59:51 +0300

    Multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    '''multiplies values by 2
+
+    Params:
+        a_dictionary: the dictionary
+
+    Return:
+        modified dict
+    '''
+    return { k: v * 2 for k, v in a_dictionary.items() }
