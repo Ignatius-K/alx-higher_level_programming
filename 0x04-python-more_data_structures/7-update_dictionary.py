2143e1c1ed26c5c80206709360f877e6725619ed
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-03-20 23:35:02 +0300

    7.Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    '''update dictionary
+
+    Params:
+        a_dictionary: the dictionary
+        key: the dict key
+        value: value to assign
+
+    Return:
+        None
+    '''
+    a_dictionary[key] = value
