73182a06ccb67429ef44006207ae3be31101c5b9
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-03-20 22:57:20 +0300

    3.Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    '''common elements in two sets
+
+    Params:
+        set_1(set): first set
+        set_2(set): second set
+
+    Return:
+        set of common elements
+    '''
+    return (set_1 & set_2)
