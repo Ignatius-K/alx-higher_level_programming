ec4056d363ff66709a696b83318ace628f206026
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-03-20 23:18:30 +0300

    5.Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    '''count keys in dictionary
+
+    Params:
+        a_dictionary: the dictionary
+
+    Return:
+        number of keys in dictionary
+    '''
+    return len(a_dictionary.keys())
