12936925db37e7d702a6618a87dcdfa74aecbc67
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-03 01:08:18 +0300

    [200~4. Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""Define a self-validating sqaure"""
+
+
+class Square:
+    """Self-validating square"""
+
+    def __init__(self, size: int = 0):
+        """create square
+
+        Args:
+            size: square size
+        """
+        self.size = size
+
+    @property
+    def size(self) -> int:
+        """get size
+
+        Return:
+            size of square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, size: int):
+        """set square size
+        Args:
+            size: square size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self) -> float:
+        """area of the square
+
+        Return:
+            area of square
+        """
+        return (self.__size ** 2)
