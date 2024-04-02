fe7f10fd70bca04f88a1efb391b36c4d0f26a88e
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-02 22:18:22 +0300

    3. Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except Exception:
         c = None
     finally:
-        print('Inside Result: {}'.format(c))
+        print('Inside result: {}'.format(c))
         return c
