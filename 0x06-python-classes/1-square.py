d3edb1a1297d9c07de8d9fbc976b1a9ed3c071f2
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-03 00:33:31 +0300

    1.Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define square with size"""
+
+
+class Square:
+    """Square with size"""
+    
+    def __init__(self, size: int):
+        """create a square
+
+        Args:
+            size: size of the square
+        """
+        self._size = size
