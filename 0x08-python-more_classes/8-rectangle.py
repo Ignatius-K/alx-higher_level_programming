4c9a2c665615d2e502f3ff3cd17a75416c33c865
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-09 00:44:00 +0300

    8.Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -77,7 +77,7 @@ class Rectangle:
         self.__height = height
 
     @staticmethod
-    def bigger_or_equal(rect_1:Rectangle, rect_2:Rectangle) -> Rectangle:
+    def bigger_or_equal(rect_1: Rectangle, rect_2: Rectangle) -> Rectangle:
         """gets bigger rectangle
 
         Note:
@@ -91,7 +91,7 @@ class Rectangle:
             rectangle: bigger rectange
         """
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance ofRectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
