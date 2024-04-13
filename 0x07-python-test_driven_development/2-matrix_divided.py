d02ab9ee5d674de4fea59a3e7db6ecdf6f7f69a2
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-13 15:45:47 +0300

    1. Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,7 +21,8 @@ def matrix_divided(matrix, div):
     validate_divisor(div)
     for row_index, row in enumerate(matrix):
         for element_index, element in enumerate(row):
-            matrix[row_index][element_index] = float(format(element / divisor), ".2f")
+            dividend = element / div
+            matrix[row_index][element_index] = float(format(dividend, ".2f"))
     return (matrix)
 
 
