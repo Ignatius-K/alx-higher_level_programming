d3c0982026363309c6978cd86364c5ffdd39dabe
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-13 12:52:52 +0300

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,7 +6,8 @@
         add_integer
 """
 
-def add_integer(a, b = 98):
+
+def add_integer(a, b=98):
     """Adds two integers
 
     Args:
@@ -19,18 +20,18 @@ def add_integer(a, b = 98):
     return (check_and_validate(a, 'a') + check_and_validate(b, 'b'))
 
 
-"""Checks the argument
-
-    Args:
-        arg (any): argument to validate
-
-    Return:
-        validated argument
-
-    Raises:
-        TypeError: if argument not int or float
-"""
 def check_and_validate(arg, arg_name):
+    """Checks the argument
+
+        Args:
+            arg (any): argument to validate
+
+        Return:
+            validated argument
+
+        Raises:
+            TypeError: if argument not int or float
+    """
     if not (isinstance(arg, int) or isinstance(arg, float)):
         raise TypeError(f"{arg_name} must be an integer")
     return (int(arg))
