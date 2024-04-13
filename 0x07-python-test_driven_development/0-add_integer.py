6f8750066a002fa578a9bbae5f14855433e5c5b9
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-13 12:03:59 +0300

    update: readme

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Arithmetic module
+
+    Methods:
+        add_integer
+"""
+
+def add_integer(a, b = 98):
+    """Adds two integers
+
+    Args:
+        a: the first integers
+        b: the second integers
+
+    Return:
+        (int) sum of a and b
+    """
+    return (check_and_validate(a, 'a') + check_and_validate(b, 'b'))
+
+
+"""Checks the argument
+
+    Args:
+        arg (any): argument to validate
+
+    Return:
+        validated argument
+
+    Raises:
+        TypeError: if argument not int or float
+"""
+def check_and_validate(arg, arg_name):
+    if not (isinstance(arg, int) or isinstance(arg, float)):
+        raise TypeError(f"{arg_name} must be an integer")
+    return (int(arg))
