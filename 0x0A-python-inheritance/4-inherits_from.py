017ba6e5503c9d2d26de45f5b0e36e46aaa3f21f
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-21 11:17:57 +0300

    4. Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Module defines util checking object instantiation"""
+
+
+def inherits_from(obj, a_class):
+    """check if object's class is subclass of a class
+
+    Args:
+        obj: The object
+        a_class: The class
+
+    Return:
+        True if obj's class is subclass of a_class else False
+    """
+    return (True if issubclass(obj.__class__(), a_class) else False)
