6714f438e69aa627b095307989eb7756b4285e47
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-21 11:42:16 +0300

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines BaseGeometry type"""
+
+
+class BaseGeometry:
+    """Define BaseGeometry type"""
+
+    def area(self):
+        """Do something
+
+        Raises:
+            Exception: If area not yet implemented
+        """
+        raise Exception("area() is not implemented")
