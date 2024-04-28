cde7e27829077fe251daedc63d2a36f0a13cd629
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-28 11:02:38 +0300

    2. Append write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -13,5 +13,5 @@ def append_write(filename="", text=""):
     Return:
         (int): Number of characters written
     """
-    with open(filename, "a+", encoding="utf-8"):
+    with open(filename, "a+", encoding="utf-8") as f:
         return (f.write(text))
