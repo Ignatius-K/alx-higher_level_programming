c0752e9fa3117fc3ce64b368792a39eb3e4b4ff2
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-28 11:06:18 +0300

    4. From JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Working with files"""
+
+import json
+
+
+def from_json_string(my_str = ""):
+    """Convert JSON string to object
+
+    Args:
+        my_str (str): The JSON string to Convert
+
+    Return:
+        (any): The object
+    """
+    return (json.loads(my_str))
