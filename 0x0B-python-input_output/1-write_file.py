8ffb977e771812084059d78380fef3a18444a0c6
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-28 10:48:18 +0300

    1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Working with files"""
+
+
+def write_file(filename="", text=""):
+    """Write to a file
+
+    Args:
+        filename (str): The path to file
+        text (str): The text to write to file
+
+    Return:
+        (int): Number of characters written
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return (f.write(text))
