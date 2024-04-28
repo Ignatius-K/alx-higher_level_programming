467028f1e2bef0c7dfebcb042526de852fa2bd2d
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-29 00:09:22 +0300

    3. Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,6 +31,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """Set width"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
     @property
@@ -41,6 +45,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """Set height"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -51,6 +59,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """Set x"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -61,4 +73,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """Assign y"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -31,3 +31,37 @@ class TestRectangle(unittest.TestCase):
         assigned_id = random.randint(111, 1111)
         rectangle = Rectangle(*self.args, id=assigned_id)
         self.assertEqual(rectangle.id, assigned_id)
+
+    def test_that_width_and_height_required(self):
+        """Width and height are required"""
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle()
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(self.args[0])
+
+    def test_width_and_height_must_be_integer(self):
+        """Rectangle width and height must be integer"""
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle("3", 4)
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(3, "4")
+
+    def test_width_and_height_must_be_greater_than_0(self):
+        """Rectangle width and height must be greater than 0"""
+        with self.assertRaises(ValueError):
+            rectangle = Rectangle(-1, 3)
+        with self.assertRaises(ValueError):
+            rectangle = Rectangle(2, -1)
+
+    def test_coords_must_be_integer(self):
+        """Rectangle x and y must be an integer"""
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(*self.args, x="1")
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(*self.args, y="1")
+
+    def test_coords_must_be_not_less_than_0(self):
+        with self.assertRaises(ValueError):
+            rectangle = Rectangle(*self.args, x=-1)
+        with self.assertRaises(ValueError):
+            rectangle = Rectangle(*self.args, y=-1)
