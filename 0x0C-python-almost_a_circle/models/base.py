aa78db40b9de59d3b69f2261f1e596f595b0c2d0
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-04-28 22:43:47 +0300

    1. Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Define the Base Type"""
+
+
+class Base:
+    """Base Shape"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Create a Base object
+
+        Args:
+            id (None | int): The Id of the object
+
+        Notes:
+            - If Id is None, then the object_number is assigned
+        """
+        if id is not None:
+            self.id = id
+            return
+        self.__class__.__nb_objects += 1
+        self.id = self.__class__.__nb_objects
+
+    def __del__(self):
+        """Destroy a Base object"""
+        self.__class__.__nb_objects -= 1
