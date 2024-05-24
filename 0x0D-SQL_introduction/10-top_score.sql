b3d85a5d698bc363d328798e266783882ec2925e
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-05-24 03:10:49 +0300

    10. List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records of table `second_table`
+-- Display `score` and `name` in this order
+-- Records ordered by score (top first)
+SELECT score, name FROM second_table
+	ORDER BY score DESC;
