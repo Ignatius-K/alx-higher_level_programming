8c737740d1aa62727d85a9dc27d3d2fb13a038f5
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-05-24 05:04:16 +0300

    4. ID can't be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,7 @@
--- Create table `force_name`
+-- Create table `id_not_null`
 -- With columns; id, name
 -- Id can't be NULL, with default of 1
-CREATE TABLE IF NOT EXISTS force_name (
+CREATE TABLE IF NOT EXISTS id_not_null (
 	id INT NOT NULL DEFAULT 1,
 	name VARCHAR(256)
 );
