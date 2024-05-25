a979bdefe675c16236bfade505367ec059d6fb07
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-05-25 12:57:25 +0300

    9. Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,10 @@
+-- List all cities in database `hbtn_0d_usa`
+-- Display; `cities.id`, `cities.name`, `states.name`
+SELECT
+	cities.id,
+	cities.name,
+	states.name
+FROM
+	cities
+LEFT JOIN
+	states ON cities.state_id = states.id
