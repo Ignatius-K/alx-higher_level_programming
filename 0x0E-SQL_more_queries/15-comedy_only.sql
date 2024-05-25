3f4797bfd83c575d97b71b370c9efd2db8263dc3
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-05-25 14:52:03 +0300

    15. Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,16 @@
+-- List comedy shows
+SELECT
+	tv_show.title
+FROM
+	tv_shows AS tv_show
+INNER JOIN
+	tv_show_genres AS show_genre
+		ON show_genre.show_id = tv_show.id
+INNER JOIN
+	tv_genres as genre
+		ON genre.id = show_genre.genre_id
+WHERE
+	genre.name = "Comedy"
+ORDER BY
+	tv_show.title ASC
+;
