e2f9e969b45b39a4eff3500fecb408c06ecf0397
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-05-25 13:27:02 +0300

    11. Genre ID for all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -7,7 +7,7 @@ SELECT
 FROM
 	tv_shows AS shows
 LEFT JOIN
-	tv_show_genres as show_genres
+	tv_show_genres AS show_genres
 		ON shows.id = show_genres.show_id
 ORDER BY
 	shows.title ASC, show_genres.genre_id ASC
