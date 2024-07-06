0a1255f2fbb64185d073091e78c493201f9e2a38
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-07 01:03:12 +0300

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+"""Script sends Request, displays body while handling errors"""
+
+import urllib.request
+from urllib.error import HTTPError
+import sys
+
+if __name__ == "__main__":
+    request_url = sys.argv[1]
+
+    request = urllib.request.Request(request_url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode())
+    except HTTPError as e:
+        print("Error code:", e.code)
