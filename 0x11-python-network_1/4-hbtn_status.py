86fd1b68be1a94a850ffc553fa4d7db75088cf54
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-07 01:19:56 +0300

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""Script that makes HTTP request"""
+
+import sys
+from requests import Request, Session
+
+if __name__ == "__main__":
+
+    request_url = sys.argv[1]
+    request = Request(method="GET", url=request_url)
+
+    session = Session()
+    prepared_request = session.prepare_request(request)
+    response = session.send(request=prepared_request)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
