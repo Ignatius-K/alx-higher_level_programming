8d69526bea7fa940b14b59447a96013871a5056b
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-07 00:49:01 +0300

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+"""Script that make request URL with an email"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+
+    request_url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {
+        'email': email
+    }
+    payload = urllib.parse.urlencode(data).encode('ascii')
+    request = urllib.request.Request(request_url, data=payload)
+
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
