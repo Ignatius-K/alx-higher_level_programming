75e989eaf290f6e5ad7d11e89e69c620037c8fe3
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-07 01:38:50 +0300

    5. Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""Script makes HTTP request and gets Header value"""
+
+import sys
+from requests import Request, Session
+
+HEADER_NAME = "X-Request-Id"
+
+if __name__ == "__main__":
+    request_url = sys.argv[1]
+
+    session = Session()
+    request = Request(method="GET", url=request_url)
+    formatted_request = session.prepare_request(request=request)
+
+    response = session.send(formatted_request)
+    print(response.headers.get(HEADER_NAME))
