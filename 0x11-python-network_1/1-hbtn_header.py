98cd89b0ec1edbead1d91b7254b08c8f7bba27e3
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-07 00:25:33 +0300

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+"""Script makes request to URL and display value of Response Header"""
+
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    REQUIRED_HEADER = "X-Request-Id"
+    URL = sys.argv[1]
+
+    with urllib.request.urlopen(URL) as response:
+        print(response.getheader(REQUIRED_HEADER))
