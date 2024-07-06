65030a1bdff4a4e9f200b27ba5defd32a7ab6b20
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-07 02:11:08 +0300

    7. Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+"""Script makes HTTP request, displays body while handling errors"""
+
+import sys
+from requests import Request, Session, exceptions
+
+if __name__ == "__main__":
+    request_url = sys.argv[1]
+
+    request = Request(
+        method="get",
+        url=request_url
+    )
+    session = Session()
+    prepared_request = session.prepare_request(request)
+
+    try:
+        response = session.send(prepared_request)
+        response.raise_for_status()
+        print(response.text)
+    except exceptions.HTTPError as e:
+        print("Error code:", e.response.status_code)
