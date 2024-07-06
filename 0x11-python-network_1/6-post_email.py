2539d3eabd7fde4ad5149af0bab664cfc446a793
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-07 01:51:17 +0300

    6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+"""Script post an email"""
+
+from requests import Request, Session
+import sys
+
+if __name__ == "__main__":
+    request_url = sys.argv[1]
+    email = sys.argv[2]
+
+    request = Request(
+        method="post",
+        url=request_url,
+        data={
+            'email': email
+        }
+    )
+
+    session = Session()
+    formatted_request = session.prepare_request(request)
+    response = session.send(request=formatted_request)
+    print(response.text)
