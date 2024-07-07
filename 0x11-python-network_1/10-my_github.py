c95b35793a50b5a23a608187e0606c93f9b7f927
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-07 03:24:37 +0300

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+"""Script that fetches Github user details"""
+
+import sys
+from requests import Request, Session, exceptions
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password_or_token = sys.argv[2]
+
+    request = Request(
+        method="get",
+        url="https://api.github.com/user",
+        auth=(username, password_or_token)
+    )
+
+    session = Session()
+    prepared_request = session.prepare_request(request=request)
+
+    response = session.send(prepared_request)
+    if (response.ok):
+        print(response.json().get("id"))
+    else:
+        print(None)
