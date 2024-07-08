6e7d76bf4a9a823af7343f75444e44bbf759e54f
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 13:49:14 +0300

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
+/*
+ * 8-square
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that print square
+ */
+const { argv, exit } = require('node:process');
 
+let squareSize;
+if (!(squareSize = parseInt(argv[2]))) {
+  console.log('Missing size');
+  exit(0);
+}
+Array(squareSize).fill().forEach(() => {
+  console.log('x'.repeat(squareSize));
+});
