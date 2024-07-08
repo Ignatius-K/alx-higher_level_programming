9b4fad28fe4ade992da64524516654a5e55bd224
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 14:33:43 +0300

    13. Add file

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-
+/*
+ * 13-add
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Module defining arithmetic addition operation
+ */
+exports.add = function add(param1, param2) {
+  return (param1 + param2)
+}
