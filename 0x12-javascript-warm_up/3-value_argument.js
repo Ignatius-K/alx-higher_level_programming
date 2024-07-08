ebb05021cefeb3182b5884e1485e9ba054f56d1d
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 12:05:19 +0300

    3. Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
-
+/*
+ * 3-value_argument
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that prints first arg
+ */
+const { argv, exit } = require('node:process');
+if (argv.length === 2) {
+  console.log('No argument');
+  exit(0);
+}
+console.log(argv[2]);
