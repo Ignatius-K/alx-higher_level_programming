504750f797e0a9e5ba263cd50d75e93fb728d9cc
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 15:30:55 +0300

    16. Add me maybe

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
+/*
+ * 102-add_me_maybe
+ * Written by Ignatius K
+ *
+ * Script override a function arg by adding 1
+ */
 
+exports.addMeMaybe = function (number, functionToCall) {
+  functionToCall(number + 1);
+};
