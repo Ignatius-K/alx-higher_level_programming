9a42d55d59fff90258954b14e552b4444abbbc69
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 15:10:15 +0300

    15. Call me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
+/*
+ * 101-call_me_moby
+ * Written by Ignatius K
+ *
+ * Module that defines a function, executing another `x` times
+ */
 
+exports.callMeMoby = function (x, functionToCall) {
+  Array(x).fill().forEach(() => functionToCall.call());
+};
