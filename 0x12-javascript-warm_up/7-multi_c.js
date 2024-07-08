8936727fa4bfd54ba954a60655fce2ea54ef3e3e
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 12:40:58 +0300

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
-
+/*
+ * 6-multi_languages
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that logs using provided occurrences
+ */
+const { argv, exit } = require('node:process');
+let number;
+if (isNaN((number = argv[2]))) {
+  console.log('Missing number of occurrences');
+  exit(0);
+}
+for (let i = 0; i < number; i++) {
+  console.log('C is fun');
+}
