111c53dbf272ad755ac2369dadb6b9e61e0b154a
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 11:29:13 +0300

    1. 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-
+/*
+ * 1-multi_languages
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script shows the best langs
+ */
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
