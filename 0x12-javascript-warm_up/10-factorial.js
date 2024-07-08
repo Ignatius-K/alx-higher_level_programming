03a8c8dde06e230da36e763e763a3b029b4180cb
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 14:14:05 +0300

    10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,21 @@
 #!/usr/bin/node
+/*
+ * 10-factorial
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that computes factorial of an arg
+ */
 
+const { argv } = require('node:process');
+
+function factorial (number) {
+  if (isNaN(number)) {
+    return 1;
+  }
+  if (number === 0) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(parseInt(argv[2])));
