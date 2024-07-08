2f94cad2d5e8ea72302938865321de4961378bf8
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 14:04:28 +0300

    9. Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
+/*
+ * 9-add
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script defines `add` for addition arithmetic
+ */
 
+const { argv } = require('node:process');
+
+function add (param1, param2) {
+  return (param1 + param2);
+}
+
+console.log(add(parseInt(argv[2]), parseInt(argv[3])));
