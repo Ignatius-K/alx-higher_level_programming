d855e93c7aa9219a56d82c1e34816f5e1c67ef41
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 11:23:05 +0300

    0. First constant, first print

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ * 0-javascript_is_amazing
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * JS Script that logs a var
+ */
+
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
