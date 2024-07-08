744175e586a7ea90b6adc8e21f85ac20f3d91944
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 12:23:45 +0300

    5. An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
-
+/*
+ * 5-to_integer
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that logs the number
+ */
+const { argv, exit } = require('node:process');
+let number;
+if ((number = parseInt(argv[2]))) {
+  console.log(`My number: ${number}`);
+  exit(0);
+}
+console.log('Not a number');
