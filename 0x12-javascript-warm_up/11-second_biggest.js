e144e1964554481280a79a6828e7aac2a9fa1270
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 14:21:59 +0300

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,18 @@
 #!/usr/bin/node
+/*
+ * 11-second_biggest
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that logs the second biggest argument
+ */
 
+const { argv, exit } = require('node:process');
+
+if (argv.length <= 3) {
+  console.log(0);
+  exit(0);
+}
+
+const args = argv.slice(2).map((arg) => parseInt(arg))
+  .sort((a, b) => b - a);
+console.log(args[1]);
