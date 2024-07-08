15aa1a3510e1c25f4c55104c73374c1676ad072f
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 12:11:38 +0300

    4. Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-
+/*
+ * 3-value_argument
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that reads provided args
+ */
+const { argv } = require('node:process');
+console.log(`${argv[2]} is ${argv[3]}`);
