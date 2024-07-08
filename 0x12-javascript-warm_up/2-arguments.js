26309868cd1567c735049e3f0362b074f802a0d1
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-08 11:41:28 +0300

    2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
-
+/*
+ * 1-multi_languages
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that target number of args
+ */
+const { argv } = require('node:process');
+if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
