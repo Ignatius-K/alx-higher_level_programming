e007b2dd1e1140781c4a6b4c867c00b4b318e6ac
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-30 20:57:45 +0300

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,2 +1,30 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+/**
+ * 1-writeme.js
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that writes string to file
+ *
+ * Requirements:
+ *  - @file: 1st argument (file to write to)
+ *  - @string: 2nd argument (string to write)
+ */
 
+const fs = require('node:fs');
+const { argv, exit } = require('node:process');
+
+const NUMBER_OF_EXPECTED_ARGS = 4;
+if (argv.length < NUMBER_OF_EXPECTED_ARGS) {
+  exit(1);
+}
+
+fs.writeFile(
+  argv[2],
+  argv[3],
+  { encoding: 'utf-8' },
+  (error) => {
+    if (error) {
+      console.log(error);
+      exit(1);
+    }
+  });
