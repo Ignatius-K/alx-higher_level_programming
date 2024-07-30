4afc4fc050ea127b15a9dbce99a8c7bd6059e96e
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-30 21:25:57 +0300

    3. Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,2 +1,27 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+/**
+ * 3-starwars_title.js
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that prints title of Star wars movie
+ *
+ * Requirements:
+ *  - @movieID: 1st argument
+ */
 
+const { argv, exit } = require('node:process');
+const request = require('request');
+
+const NUM_OF_EXPECTED_ARGS = 3;
+if (argv.length < NUM_OF_EXPECTED_ARGS) {
+  exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
+request(url, {}, (error, __, body) => {
+  if (error) {
+    console.log(error);
+    exit(1);
+  }
+  console.log(JSON.parse(body).title);
+});
