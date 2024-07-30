e7100d69e15ea715a08fabd36d0486a30c865018
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-30 20:45:25 +0300

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,25 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+/**
+ * 0-readme.js
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that reads and print the content of file
+ *
+ * Requirement:
+ * - @file: 1st argument
+ */
 
+const fs = require('node:fs');
+const { argv, exit } = require('node:process');
+
+if (argv.length < 3) {
+  exit(1);
+}
+
+fs.readFile(argv[2], { encoding: 'utf-8' }, (error, data) => {
+  if (error) {
+    console.log(error);
+    exit(1);
+  }
+  console.log(data);
+});
