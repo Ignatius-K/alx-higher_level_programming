1e92415bf38569f94af75a77d828f14b7f96e16a
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-30 23:13:02 +0300

    7. Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,71 +6,49 @@
  * Script that prints characters of Star Wars movie
  */
 
-const { argv, exit } = require('node:process')
-const request = require('request')
+const { argv, exit } = require('node:process');
+const request = require('request');
 
-let NUM_OF_EXPECTED_ARGS = 3
+const NUM_OF_EXPECTED_ARGS = 3;
 if (argv.length < NUM_OF_EXPECTED_ARGS) {
-  console.error(`Usage: ${argv[1]} {movie_id}`)
-  exit(1)
+  console.error(`Usage: ${argv[1]} {movie_id}`);
+  exit(1);
 }
 
-let MOVIE_ID = argv[2]
-let url = `https://swapi-api.alx-tools.com/api/films/${MOVIE_ID}/`
+const MOVIE_ID = argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${MOVIE_ID}/`;
 request(url, {}, (error, response, body) => {
   if (error) {
-    console.log(error)
-    return
+    console.log(error);
+    return;
   }
 
   if (response.statusCode !== 200) {
-    console.info(`Failed with code: ${response.statusCode}`)
-    return
+    console.info(`Failed with code: ${response.statusCode}`);
+    return;
   }
 
-  let characters = JSON.parse(body)['characters']
-  characters.forEach(characterUrl => handleQueryCharacter(characterUrl))
-})
-
+  const characters = JSON.parse(body).characters;
+  characters.forEach(characterUrl => handleQueryCharacter(characterUrl));
+});
 
 /**
  * @param {string} characterUrl
  * @returns void
  */
-let handleQueryCharacter = (characterUrl) => {
+const handleQueryCharacter = (characterUrl) => {
   if (!characterUrl) {
-    return
+    return;
   }
   request(characterUrl, {}, (error, response, body) => {
     if (error) {
-      return
+      return;
     }
 
     if (response.statusCode !== 200) {
-      return
+      return;
     }
 
-    console.log(JSON.parse(body).name)
-  }) 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log(JSON.parse(body).name);
+  });
+};
