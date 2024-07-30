29075166ae8aadf6181989adb714e1ca03808fc4
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-30 21:12:26 +0300

    2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,2 +1,27 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+/**
+ * 2-statuscode.js
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that displays the status code of `GET` request
+ *
+ * Requirements:
+ *   - @url: 1st argument (URL to request)
+ *   - request: module to be used
+ */
 
+const request = require('request');
+const { argv, exit } = require('node:process');
+
+const NUM_OF_EXPECTED_ARGS = 3;
+if (argv.length < NUM_OF_EXPECTED_ARGS) {
+  exit(1);
+}
+
+request(argv[2], { method: 'GET' }, (error, response) => {
+  if (error) {
+    console.log(error);
+    exit(1);
+  }
+  console.log(`code: ${response.statusCode}`);
+});
