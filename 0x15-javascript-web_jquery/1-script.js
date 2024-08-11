6c88aa35cd47ab34f507b9ec050cf3ad088297c7
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-08-11 09:11:33 +0300

    1. With JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,12 @@
+/**
+ * 1-script.js
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that updates text color of the <header>
+ *
+ * Requirements:
+ *  - JQuery
+ */
+
+// eslint-disable-next-line no-undef
+$('header').css('color', '#FF0000');
