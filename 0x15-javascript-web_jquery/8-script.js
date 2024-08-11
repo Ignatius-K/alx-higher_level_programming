a605efee7367829007be11db8e4cd9dccfd51835
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-08-11 11:33:08 +0300

    8. Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,31 @@
+/**
+ * 8-script.js
+ * Written by Ignatius K <ignatiuskisekka@gmail.com>
+ *
+ * Script that fetches and lists the title for all movies
+ * lists them in the <UL#list_movies>
+ *
+ * Requirements:
+ *  - JQuery
+ */
+
+/* eslint-disable no-undef */
+const movieList = $('UL#list_movies');
+
+if (movieList) {
+  $.ajax({
+    method: 'GET',
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    dataType: 'json'
+
+  }).done((data) => {
+    const tempList = $('<ul/>');
+
+    data?.results.forEach((movie) => {
+      const tempLi = $('<li/>').text(movie?.title ? movie?.title : 'No title');
+      tempList.append(tempLi);
+    });
+    movieList.html(tempList.html());
+  });
+}
+/* eslint-disable no-undef */
